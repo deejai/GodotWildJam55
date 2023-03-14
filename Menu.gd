317e47ea5048e1f3c706b7dac46806ddb70040d2
strat1227 <strat1227@gmail.com>
2023-03-14 16:02:09 -0500

    Levels updated

---
+++ Menu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+var scene = load("res://Levels/Level33.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_packed(scene)
