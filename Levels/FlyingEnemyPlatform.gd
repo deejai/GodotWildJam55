c2da6df90c63f85a522ff5bf9da29e35a667edd2
deejai <deejai@live.com>
2023-03-16 17:15:36 -0600

    idk i forgot

--- Levels/FlyingEnemyPlatform.gd
+++ Levels/FlyingEnemyPlatform.gd
@@ -1,11 +1,22 @@
-extends AnimatableBody2D
+extends StaticBody2D
 
+@onready var leftbound: Vector2 = $LeftBound.global_position
+@onready var rightbound: Vector2 = $RightBound.global_position
+
+var speed: float = 100.0
+var direction: Vector2 = Vector2.RIGHT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$AnimationPlayer.play("LeftRight")
+	position += direction * speed * delta
+
+	if direction == Vector2.RIGHT:
+		if position.x > rightbound.x:
+			direction = Vector2.LEFT
+	elif direction == Vector2.LEFT:
+		if position.x < leftbound.x:
+			direction = Vector2.RIGHT
--- Levels/fillpolygon.gd
+++ Levels/fillpolygon.gd
@@ -1,15 +1,4 @@
 extends CollisionPolygon2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _draw():
 	draw_polygon(polygon, PackedColorArray([Color.DARK_RED]))
